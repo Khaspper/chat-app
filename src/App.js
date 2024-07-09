1a3e3de5c543d0f79744d00a6c49a22412f68375
Mark Narciso <mnarciso@decentrelabs.com>
2024-07-09 02:20:51 -0700

    We can now keep track of the room

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import React, { useState, useRef } from 'react';
 import './App.css';
 import { Auth } from './components/auth';
 
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
+
 function App() {
-  return <div className="App"> <Auth /> </div>;
+  const [isAuth, setIsAuth] = useState(cookies.get('auth-token'))
+  const [room, setRoom] = useState(null)
+
+  const roomInputRef = useRef(null)
+
+  if (!isAuth) {
+  return (
+    <div className="App"> 
+      <Auth />
+    </div>)
+  }
+  
+  return (
+    <div>
+      {room ? (
+        <div> Chat </div>
+      ) : (
+        <div className='room'>
+          <label>Enter Room Name:</label>
+          <input ref={roomInputRef}/>
+          <button onClick={() => setRoom(roomInputRef.current.value)}> Enter Chat</button>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default App;
